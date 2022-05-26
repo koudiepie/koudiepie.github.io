3f9e1434a51c37a6c72a3959476b0c2bdefae605
koudiepie <91250563+koudiepie@users.noreply.github.com>
2022-05-26 17:47:27 +0800

    add food week13

---
+++ food_13/food_13.js
@@ -0,0 +1,35 @@
+function setup() {
+    initializeFields();
+    createCanvas(400, 300);
+    textSize(40);
+}
+
+var choice;
+
+function draw() {
+    background(color(0xEA, 0x45, 0x45));
+    fill(color(0xDF, 0xEA, 0x45));
+    if (choice == 0)
+        rect(50, 50, 150, 2);
+    if (choice == 1)
+        rect(50, 100, 150, 2);
+    if (choice == 2)
+        rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
+    text("中餐吃泡麵 ", 50, 50);
+    text("中餐UberEat ", 50, 100);
+    text("中餐FoodPanda ", 50, 150);
+    text("中餐吃牛排", 50, 200);
+    text("中餐吃大餐", 50, 250);
+}
+
+function mousePressed() {
+    choice = int(random(5));
+}
+
+function initializeFields() {
+    choice = -1;
+}
